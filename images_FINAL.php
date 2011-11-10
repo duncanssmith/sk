b18685cce110336065f05d5b9ec63baaee04d6ad
Duncan Smith <duncanssmith@gmail.com>
2011-11-10 21:45:22 +0000

    Final image implementation
    There are still a few to do tho! argh!

--- images_FINAL.php
+++ images_FINAL.php
@@ -1,4 +1,5 @@
 <?php
+
 $images=array(
 '0'=>'iStock_000010645616XSmall.jpg',
 '1'=>'iStock_000014958241XSmall.jpg',
@@ -7,6 +8,8 @@ $images=array(
 '7'=>'iStock_000012888270XSmall.jpg',
 '8'=>'iStock_000013078634XSmall.jpg',
 '9'=>'iStock_000007353693XSmall.jpg',
+'10'=>'iStockPhoto_104080606_8.png',
+'11'=>'iStockPhoto_104080606_4.png',
 '14'=>'iStock_000011734855XSmall.jpg',
 '16'=>'iStock_000012257917XSmall.jpg',
 '17'=>'iStock_000014769178XSmall.jpg',
